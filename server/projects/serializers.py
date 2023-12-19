a1445ebb82dc6d3cf451f089300d448a9ff8f963
anurag sarkar <sarkar.anurag@outlook.com>
2023-12-19 21:26:53 +0530

    added serialzer

---
+++ server/projects/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from projects.models import Project, Task, Comment
+from users.models import Team, User
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
